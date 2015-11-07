55c08958cbe37ae807b9386ea52cbb4cb42deec8
Darrel Herbst <dherbst@gmail.com>
2015-11-07 10:32:17 -0500

    simple example webpage

---
+++ src/gcsexample/handlers.go
@@ -0,0 +1,13 @@
+package gcsexample
+
+import (
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", IndexHandler)
+}
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/index.html", http.StatusFound)
+}
